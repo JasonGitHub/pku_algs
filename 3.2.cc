e27d0bfd914983a9a482a3c76aa70507538457f3
jason <jasonwu2011@gmail.com>
2013-11-17 16:35:17 +0800

    update

---
+++ 3.2.cc
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <string>
+#include <deque>
+
+using namespace std;
+
+void PrintStack(deque<int> s) {
+  for (deque<int>::iterator it = s.begin(); it != s.end(); ++it) {
+    cout << *it << " ";
+  }
+  cout << endl;
+}
+
+int main() {
+  int m, n;
+  cin >> m;
+  string op;
+  for (int i = 0; i < m; ++i) {
+    deque<int> s;
+    cin >> n;
+    bool err = false;
+    for (int j = 0; j < n; ++j) {
+      cin >> op;
+      if (op == "push") {
+        int val;
+        cin >> val;
+        s.push_back(val);
+      } else {
+        if (s.empty()) {
+          err = true;
+        } else s.pop_back();
+      }
+    }
+    if (err) cout << "error" << endl;
+    else if (!s.empty()) PrintStack(s);
+  }
+  return 0;
+}
